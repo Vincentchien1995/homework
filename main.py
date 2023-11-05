c6923881d338b1d07d2c24d1a4a4106d20ddbec3
Vincentchien1995 <sheepjessie@gmail.com>
2023-11-05 16:23:02 +0100

    renew

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
-def is_prime(N):
+def is_prime(N):  #Define is_prime function
     if N <=1:
         return False
     for i in range(2,N):
-        if N%i == 0:
+        if N%i == 0:   #if remainder equal to 0 then it's not a prime_number
             return False
     return True
 
 N =int(input("enter the value of N"))
 def n_prime(N):
-    num = 2
-    count =0
+    num = 2  #prime_number start from 2
+    count =0 #count how many prime_number
     while count<N:
         if is_prime(num):
             print(num)
@@ -21,21 +21,3 @@ print(n_prime(N))
 
 
 
-def simple_search(tab, item):
-    i = 0
-    while i < len(tab):
-        if tab[i] == item:
-        return i
-        i += 1
-    return -1
-
-
-nitems = int(input("Enter the number of items: "))
-items = []
-i = 0
-while i < nitems:
-    item = float(input("Enter the item: "))
-    items.append(item)
-    i += 1
-
-print(simple_search(items,4))
